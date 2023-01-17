d4ea084c581aeac0684e4c97597c6a83425c424b
jjia-tn <jjia.tn@gmail.com>
2023-01-17 14:42:03 +0800

    added server socket connection

--- App.java
+++ App.java
@@ -1,6 +1,13 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Hello world!
@@ -12,8 +19,9 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         String dirPath = "data2";
         File newDirectory = new File(dirPath);
@@ -22,6 +30,30 @@ public final class App {
             System.out.println("Directory already exists");
         } else {
             newDirectory.mkdir();
+        }
+
+        Cookie cookie = new Cookie();
+        cookie.readCookieFile();
+        cookie.showCookies();
+
+        ServerSocket ss = new ServerSocket(7000);
+        Socket s = ss.accept(); // establish connection and wait for client to connect
+
+        try (InputStream is = s.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+            String msgReceived = "";
+
+            while (!msgReceived.equals("close")) {
+                msgReceived = dis.readUTF();
+
+                if (msgReceived.equalsIgnoreCase("get-cookie")) {
+                    String cookieValue = cookie.returnCookie();
+                }
+            }
+        } catch (EOFException ex) {
+            s.close();
+            ss.close();
         }
     }
 }
